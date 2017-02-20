4327aa21d4012a703966e08fd020b563d660036f
Sunil Sadasivan <sunil@buffer.com>
2017-02-20 11:48:22 -0800

    Get a simple header bar up

--- src/client/components/HeaderBar/HeaderBar.jsx
+++ src/client/components/HeaderBar/HeaderBar.jsx
@@ -3,9 +3,28 @@ import React from 'react'
 import './HeaderBar.css';
 
 const HeaderBar = () => (
-  <div className="app-header">
-    <div>Overview </div>
-  </div>
+
+<nav className="navbar navbar-default navbar-fixed-top">
+      <div className="container">
+        <div className="navbar-header">
+          <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span className="sr-only">Toggle navigation</span>
+            <span className="icon-bar"></span>
+            <span className="icon-bar"></span>
+            <span className="icon-bar"></span>
+          </button>
+        </div>
+        <div id="navbar" className="navbar-collapse collapse">
+          <ul className="nav navbar-nav">
+            <li className="active"><a href="#">Overview</a></li>
+            <li><a href="#about">Comparison</a></li>
+          </ul>
+          <ul className="nav navbar-nav navbar-right">
+            <div className='logo'>M</div>
+          </ul>
+        </div>
+      </div>
+    </nav>
 );
 
 module.exports = HeaderBar;
