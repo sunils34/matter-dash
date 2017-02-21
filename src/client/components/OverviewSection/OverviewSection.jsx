9d8c703730a1d1c29a69a32667113d7e72c1430a
Sunil Sadasivan <sunil@buffer.com>
2017-02-21 15:39:28 -0800

    Get the second pie chart working

--- src/client/components/OverviewSection/OverviewSection.jsx
+++ src/client/components/OverviewSection/OverviewSection.jsx
@@ -51,16 +51,25 @@ class OverviewSectionHeader extends React.Component {
 const data = [{name: 'Male', value: 70}, {name: 'Female', value: 10},
                     {name: 'Prefer not to disclose', value: 2}, {name: 'Non-Binary', value: 8}];
 
+const ethData = [
+  {name: 'White', value: 57}, 
+  {name: 'Native American/Alaskan Native', value: 2},
+  {name: 'Native Hawaiian/Pacific Islander', value: 2},
+  {name: 'Hispanic', value: 8},
+  {name: 'Black/African American', value: 9},
+  {name: 'Asian', value: 18}
+];
+
 class OverviewCharts extends React.Component {
 
   render() {
     return (
       <div className='row'>
         <div className='col-lg-6'>
-          <MatterPieChart data={data} title="Gender"/>
+          <MatterPieChart data={data} legendAlign='left' title="Gender"/>
         </div>
         <div className='col-lg-6'>
-
+          <MatterPieChart data={ethData} legendAlign='right' title="Ethnicity"/>
         </div>
       </div>
     );
