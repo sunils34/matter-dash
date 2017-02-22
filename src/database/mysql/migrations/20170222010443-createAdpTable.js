fde5233bcb48f6f2c5b2f3e949a9aa3268067d37
Sunil Sadasivan <sunil@buffer.com>
2017-02-21 18:37:08 -0800

    Get boilerplate working for mysql database. Set up migration schema too

---
+++ src/database/mysql/migrations/20170222010443-createAdpTable.js
@@ -0,0 +1,58 @@
+'use strict';
+
+const adpTableName = 'adp';
+
+module.exports = {
+  up: function (queryInterface, Sequelize) {
+
+
+    /*
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.createTable('users', { id: Sequelize.INTEGER });
+    */
+
+    queryInterface.createTable(
+      adpTableName,
+      {
+        id: {
+          type: Sequelize.BIGINT,
+          primaryKey: true,
+          autoIncrement: true
+        },
+        orgId: Sequelize.STRING,
+        firstName: Sequelize.STRING,
+        lastName: Sequelize.STRING,
+        positionStatus: Sequelize.STRING,
+        hireDate: {
+          type: Sequelize.DATE
+        },
+        jobFunction: Sequelize.STRING,
+        payGradeCode: Sequelize.STRING,
+        location: Sequelize.STRING,
+        age: Sequelize.STRING,
+        eeoEthnicDescription: Sequelize.STRING,
+        gender: Sequelize.STRING,
+        createdAt: {
+          type: Sequelize.DATE
+        },
+        updatedAt: {
+          type: Sequelize.DATE
+        }
+      }
+    )
+  },
+
+  down: function (queryInterface, Sequelize) {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.dropTable('users');
+    */
+    queryInterface.dropTable(adpTableName);
+  }
+};
