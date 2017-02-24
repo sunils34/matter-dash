7a5a602fa6d2940b7796f89e183a4b0fd0200693
Sunil Sadasivan <sunil@buffer.com>
2017-02-23 16:25:50 -0800

    Fix production redux store

--- src/client/redux/store/store.prod.js
+++ src/client/redux/store/store.prod.js
@@ -2,7 +2,7 @@ import {createStore, applyMiddleware, compose} from 'redux';
 import rootReducer from '../reducers/index';
 import thunk from 'redux-thunk';
 import promiseMiddleware from '../middleware/promiseMiddleware';
-import { reduxTimeout } from '../../utils/timeout';
+import { reduxTimeout } from '../utils/timeout';
 
 const finalCreateStore = compose(
     applyMiddleware(thunk, promiseMiddleware, reduxTimeout())
