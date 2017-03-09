6cbfa30ab396acefe09fd2ef8b90b5b38c651db0
Sunil Sadasivan <sunil@buffer.com>
2017-03-09 10:58:56 -0800

    Add primary grey color

--- src/server/graphql/queries/fields.js
+++ src/server/graphql/queries/fields.js
@@ -1,6 +1,6 @@
 import _ from 'lodash';
 
-const COLORS = ['#6E6EE2', '#72D5C6', '#F1BA00', '#E96DA4', '#E28D6E', '#3DBAEF', '#3481A5'];
+const COLORS = ['#DEDEDE', '#72D5C6', '#6E6EE2', '#F1BA00', '#E96DA4', '#E28D6E', '#3DBAEF', '#3481A5'];
 
 const getFields = async (type, orgId, sequelize) => {
   let fields = await sequelize.query(`SELECT ${type} as name FROM employees where orgId=$orgId GROUP BY ${type} ORDER BY count(*) DESC`, {
