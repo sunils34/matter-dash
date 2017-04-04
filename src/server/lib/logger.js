7685f167f9b4463599f77ca44093c1f1ba9a758a
Sunil Sadasivan <sunil@buffer.com>
2017-04-03 22:46:59 -0700

    Add logging for signin and create user.  Also serialize json to single line

--- src/server/lib/logger.js
+++ src/server/lib/logger.js
@@ -1,10 +1,17 @@
 import winston from 'winston';
+import { serialize } from 'winston/lib/winston/common';
 
-const formatter = options => (
-  // Return string will be passed to logger.
-  winston.config.colorize(options.level,' matter-app ' +  options.level.toUpperCase() + ' ' + (options.message ? options.message : '') +
-    (options.meta && Object.keys(options.meta).length ? '\n\t' + JSON.stringify(options.meta) : '') + ' ')
+const jsonFormatter = (key, value) => (
+  value instanceof Buffer
+    ? value.toString('base64')
+    : value
 );
+
+const formatter = options => {
+  // Return string will be passed to logger.
+  return winston.config.colorize(options.level,' matter-app ' +  options.level.toUpperCase() + ' ' + (options.message ? options.message : '') +
+     ((options.meta && Object.keys(options.meta).length) ?  ' ' + serialize(options.meta) : '') + ' ');
+};
 
 winston.remove(winston.transports.Console);
 winston.add(winston.transports.Console, {
