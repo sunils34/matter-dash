71ea9a9d119d5e421b1cc411bd8d04d5336e778c
Sunil Sadasivan <sunil@matterapp.io>
2017-04-13 09:49:29 -0700

    Fix index.js so that it's eslinted

--- src/server/index.js
+++ src/server/index.js
@@ -17,7 +17,7 @@ import logger from './lib/logger';
 const SequelizeStore = sequelizeStore(session);
 
 const env = process.env.NODE_ENV === 'production' ? 'prod' : 'dev';
-const appdir = path.resolve(__dirname + '/../../build/' + env);
+const appdir = path.resolve(path.join(__dirname, `/../../build/${env}`));
 
 const app = express();
 
@@ -38,94 +38,85 @@ const sessionDetails = {
 };
 
 if (env === 'prod') {
-  app.set('trust proxy', 1)
+  app.set('trust proxy', 1);
   sessionDetails.cookie.secure = true;
 }
 
 app.use(session(sessionDetails));
-app.set('views', __dirname + '/views');
+app.set('views', path.join(__dirname, '/views'));
 app.engine('html', require('ejs').renderFile);
 
 // set up server side react view engine
 app.set('view engine', 'jsx');
 app.engine('jsx', expressReactViews.createEngine());
 
+// Serve static assets from client and server public directories
+app.use(express.static(path.join(appdir, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/dist/core.js', (req, res) => {
+  res.header('Content-Type', 'application/javascript');
+  res.send('');
+});
+
+app.get('/dist/bundle*', (req, res) => {
+  if (!(env === 'dev')) {
+    res.setHeader('Cache-Control', 'public, max-age=31536000');
+  }
+  const bundleDir = appdir + req.path;
+  res.sendFile(bundleDir);
+});
+
+app.get('/dist/bundle*.js.map', (req, res) => {
+  // Set cache on everything except local development
+  if (!(env === 'dev')) {
+    res.setHeader('Cache-Control', 'public, max-age=31536000');
+  }
+  const bundleDirMap = appdir + req.path;
+  res.sendFile(bundleDirMap);
+});
+
 
 // set up oauth and login middleware
 configureAuth(app);
 
 app.get('/signin', (req, res) => {
-  if(req.isAuthenticated()) {
-    //redirect to the app
+  if (req.isAuthenticated()) {
+    // redirect to the app
     res.redirect('/');
-  }
-  else {
-    res.render('pages/signin.ejs')
+  } else {
+    res.render('pages/signin.ejs');
   }
 });
 
 app.use('/graphql', expressGraphQL((req, res) => {
-
-  if(!req.isAuthenticated()) {
+  if (!req.isAuthenticated()) {
     logger.warn('Un-signed in access to /graphql');
-    res.json({'error': 'This endpoint is only accessible to a logged in user'})
-  } else {
-    return {
-      schema,
-      graphiql: process.env.NODE_ENV == 'development',
-      rootValue : {request: req},
-      context: req.session,
-      pretty: true
-    }
+    res.json({ error: 'This endpoint is only accessible to a logged in user' });
+    return false;
   }
+  return {
+    schema,
+    graphiql: process.env.NODE_ENV === 'development',
+    rootValue: { request: req },
+    context: req.session,
+    pretty: true,
+  };
 }));
 
-// Serve static assets from client and server public directories
-app.use(express.static(path.join(appdir, 'public')));
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.get('/dist/core.js', function (req, res) {
-  res.header("Content-Type", "application/javascript");
-  const codejs = "window.SLACK_CLIENT_ID=\"" + "TEST" + "\";\n";
-  res.send(codejs);
-});
-
-app.get('/dist/bundle*', function (req, res) {
-  if (!(env == 'dev')) {
-    res.setHeader('Cache-Control', 'public, max-age=31536000');
-  }
-  var path = req.path;
-  var bundleDir = appdir + path;
-  res.sendFile(bundleDir);
-});
-
-app.get('/dist/bundle*.js.map', function (req, res) {
-  // Set cache on everything except local development
-  if (!(env == 'dev')) {
-    res.setHeader('Cache-Control', 'public, max-age=31536000');
-  }
-  var path = req.path;
-  var bundleDirMap = appdir + path;
-  res.sendFile(bundleDirMap);
-});
-
-
-
-app.get('*', function (req, res) {
-
-  if(!req.isAuthenticated()) {
-    logger.info("user unauth redirect");
+app.get('*', (req, res) => {
+  if (!req.isAuthenticated()) {
+    logger.info('user unauth redirect');
     res.redirect('/signin');
-  }
-  else {
-    var indexPath = appdir + '/index.html';
+  } else {
+    const indexPath = `${appdir}/index.html`;
     res.sendFile(indexPath);
   }
 });
 
 const port = process.env.PORT || 4000;
 
-//sync tables
-sequelizeTables.sync({force: false}).catch(err => console.error(err.stack)).then(() => {
-  app.listen(port, () => logger.info('Now browse to localhost:'+port+'/graphql'));
+// sync tables
+sequelizeTables.sync({ force: false }).catch(err => logger.error(err.stack)).then(() => {
+  app.listen(port, () => logger.info(`Now browse to localhost:${port}/graphql`));
 });
